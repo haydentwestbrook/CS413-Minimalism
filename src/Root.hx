c4e3ba3831eb473344cfd0a00e6d3596164dfa8b
ajs476 <ajs476@nau.edu>
2015-01-27 13:57:23 -0700

    Added score visibility
    Score printed and updated on game screen, game no longer allows itself
    to go below speed 1 (crashes if it does)

--- src/Root.hx
+++ src/Root.hx
@@ -7,7 +7,10 @@ import starling.events.KeyboardEvent;
 import starling.events.TouchEvent;
 import starling.events.Touch;
 import starling.events.TouchPhase;
+import starling.text.TextField;
 import Std;
+import flash.events.Event;
+
 
 
 class Root extends Sprite {
@@ -19,7 +22,10 @@ class Root extends Sprite {
 	public var square2:Image;
 	public var rand1:Int;
 	public var rand2:Int;
-    public var speed = 5;
+    public var speed:Int = 4;
+    public var points:Int = 0;
+    public var scoreText:TextField;
+    
     
 
 	public function new() {
@@ -44,7 +50,8 @@ class Root extends Sprite {
                         alpha: 0,
                         onComplete: function() {
                         startup.removeChild(startup.loadingBitmap);
-                        run(speed);
+                        
+                        listenerInit();
                			}
 
                 });
@@ -52,28 +59,43 @@ class Root extends Sprite {
 
         });
     }
+    public function displayScore(){
+        // creates score text field
+        scoreText = new TextField(500, 500, "Score: ", "Arial", 24, 0xff0000);
+        this.addChild(scoreText);
+        scoreText.text = "Score " + points;
+    }
     
- 
-    public function run(speed:Int) {
-        
-      
-        //Responsible for main game loop
-        
-        
+    public function updateScore(){
+        // removes old score value
+        this.removeChild(scoreText);
+    }
+
+
+    public function nextLevel(){
+        trace("next level reached.");
+    }
+    
+    public function listenerInit(){
+        // function adds listeners once so they are not looped
+        Starling.current.stage.addEventListener(KeyboardEvent.KEY_DOWN, keyPress);
+        Starling.current.stage.addEventListener(TouchEvent.TOUCH, touchPress);
+
+        run(speed);
+    }
+    public function run(speed) {
+        // updates then displays users current score
+        updateScore();
+        displayScore();
+
 
         //Set up squares
+        
     	makeSquare1();
     	makeSquare2();
-
-
+        
         Starling.juggler.repeatCall(makeSquare1, speed, 0);
 
-        //User input listeners
-
-    	Starling.current.stage.addEventListener(KeyboardEvent.KEY_DOWN, keyPress);
-    	Starling.current.stage.addEventListener(TouchEvent.TOUCH, touchPress);
-
-        
 
     };
 
@@ -88,7 +110,7 @@ class Root extends Sprite {
     	} else if(rand1 == 2) {
     		square1 = new Image(Root.assets.getTexture("greenSquare"));
     	}
-    	square1.x = 100;
+    	square1.x = 500;
     	square1.y = 100;
     	addChild(square1);
     }
@@ -104,7 +126,7 @@ class Root extends Sprite {
         } else if(rand2 == 2) {
             square2 = new Image(Root.assets.getTexture("greenSquare"));
         }
-        square2.x = 100;
+        square2.x = 500;
         square2.y = 500;
         addChild(square2);
     }
@@ -123,17 +145,32 @@ class Root extends Sprite {
 	 	checkWin();
 	}
 
+
+   
+    
+
+
     public function checkWin() {
         trace(rand1);
         trace(rand2);
-        if(rand1 == rand2) {
-            //Won the game
-            run(speed--);
-        } else {
-            //Game over
+        if(rand1 == rand2){
 
+            trace("match");
+            points++;
+            if(speed == 1){
+                // player at lowest speed already, if speed of 0 or -1 is reached, it will crash, so go to next level?
+                trace("speed at "+speed);
+                nextLevel();
+            }
+            else{
+                // match made and speed above 1, so runs loop again but with faster speed
+                trace("speed changed to" + (speed-1));
+                run(speed--);  
+            }   
+        }
+        else{
+            // stays in loop with no change in speed
+            trace("no match");
         }
     }
-
-
 }
